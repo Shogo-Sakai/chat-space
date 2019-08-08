d34275c419cfc3da6f4a2f699366b1b393d39474
ShowGo_JP <shogosakai73@gmail.com>
2019-08-08 19:56:07 +0900

    Update production.rb

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,3 @@
-server '52.194.73.69', user: 'ec2-user', roles: %w{app db web}
-
 # server-based syntax
 # ======================
 # Defines a single server with a list of roles and multiple properties.
@@ -8,6 +6,7 @@ server '52.194.73.69', user: 'ec2-user', roles: %w{app db web}
 # server "example.com", user: "deploy", roles: %w{app db web}, my_property: :my_value
 # server "example.com", user: "deploy", roles: %w{app web}, other_property: :other_value
 # server "db.example.com", user: "deploy", roles: %w{db}
+server '52.194.73.69', user: 'ec2-user', roles: %w{app db web}
 
 
 
