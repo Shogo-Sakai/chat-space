ed2eb4fb5890d3930c039c6b0fcc92e8823a230a
shogosakai <shogosakai73@gmail.com>
2019-09-08 11:00:14 +0900

    updated nokogiri

--- app/controllers/api/messages_controller.rb
+++ app/controllers/api/messages_controller.rb
@@ -1,5 +1,7 @@
 class Api::MessagesController < ApplicationController
   def index
+
+    # @messages = Message.where('id > ?', params[:id])
     respond_to do |format|
       format.html
       format.json{@messages = Message.where('id > ?', params[:id])}
